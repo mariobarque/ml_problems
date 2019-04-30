bfa7db76affc61b681adb16644e2fc4c92867159
mariobarque <mariobarque@gmail.com>
2019-04-30 11:10:36 -0600

    Decision Tree

---
+++ desicion_tree.py
@@ -0,0 +1,41 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+
+def convert(data):
+    number = LabelEncoder()
+    data['Alt'] = number.fit_transform(data.Alt)
+    data['Bar'] = number.fit_transform(data.Bar)
+    data['Fri'] = number.fit_transform(data.Fri)
+    data['Hun'] = number.fit_transform(data.Hun)
+    data['Pat'] = number.fit_transform(data.Pat)
+    data['Price'] = number.fit_transform(data.Price)
+    data['Rain'] = number.fit_transform(data.Rain)
+    data['Res'] = number.fit_transform(data.Res)
+    data['Type'] = number.fit_transform(data.Type)
+    data['Est'] = number.fit_transform(data.Est)
+    return data
+
+
+enc = OneHotEncoder(handle_unknown='ignore')
+df = pd.read_csv('data.csv')
+df = convert(df)
+
+model = DecisionTreeClassifier()
+le = LabelEncoder()
+
+x = df.drop('Est', axis=1)
+y = df['Est']
+
+model.fit(x, y)
+
+y_predict = model.predict(x)
+
+print(y_predict)
+
+
